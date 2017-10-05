329b98bb2530270409fcdb8c2cc9a9b6979676ab
75301 Woonyong Chung <WC75301@kids.djusd.k12.ca.us>
2017-10-04 20:36:36 -0700

    I completed Lesson1

---
+++ lesson1/lesson1.py
@@ -0,0 +1,11 @@
+name = raw_input("What is your name: ")
+
+age = raw_input("What is your age: ")
+
+ageMonths = int(age)  * 12
+
+print "Hi " + name + ", you are " + str(age) +  " years old. You are " + str(ageMonths) + " months old."
+
+
+
+
