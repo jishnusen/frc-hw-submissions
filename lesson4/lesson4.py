f9a4399646c2b72ceb42ca30191681039b9c6ab1
Teo Honda-Scully <teohondascully@gmail.com>
2017-10-04 20:47:02 -0700

    completed assignment cuatro

---
+++ lesson4/lesson4.py
@@ -0,0 +1,26 @@
+age = int(raw_input("ur age pls\n"))
+if age >= 35: 
+	print "you can b presidente del amuricas norte states unity"
+	print "u can drive carro n get a trabajo"
+	print "u can go to colegio"
+	print "ur adulto"
+	print "u can b en robot team"
+elif age >= 21:
+	print "u can drive carro n get a trabajo"
+	print "u can go to colegio"
+	print "ur adulto"
+	print "u can b en robot team"
+elif age >= 18:
+	print "u can b en robot team"
+	print "u can drive carro n get a trabajo"
+	print "u can go to colegio"
+elif age >= 16:
+	print "u can b en robot team"
+	print "u can drive carro n get a trabajo"
+elif age >= 14:
+	print "u can b en robot team"
+else:
+	print "boi"
+
+
+
