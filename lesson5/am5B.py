d01e91ef048a678da2d04488686c22e32fb87312
59846 Carl Csaposs <CC59846@kids.djusd.k12.ca.us>
2017-10-04 19:53:52 -0700

    Finished assign 5

---
+++ lesson5/am5B.py
@@ -0,0 +1,6 @@
+name = raw_input("Name: ")
+	color = raw_input("Favorite color? ")
+	petn = raw_input("Number of pets: ")
+	ar1 = [name, color, petn]
+
+	print(ar1[0] + "'s favorite color is " + ar[1] + ". They have " + ar[2] + " pets.")
