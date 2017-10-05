fd49d8a5bf5d2f60f4547ffa530673c966bf2a94
Stan Bair <stanbair@Stans-MacBook-Air.local>
2017-10-04 20:28:15 -0700

    Lesson 2 HW

---
+++ lesson2/Lesson 2 HW.py
@@ -0,0 +1,5 @@
+name = raw_input("What is your name?")
+num = raw_input("How many pizzas would you like?")
+pep = raw_input("How many pepperonis would you like?")
+olive = raw_input("How many olives would you like?")
+print name + " ordered " + num + " pizzas with " + pep + " pepperonis and " + olive + " olives."
