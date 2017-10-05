091d7129ace9db5739ece977b22b4cc0c263ba97
59254 Laura Rae Duffy <LD59254@kids.djusd.k12.ca.us>
2017-10-04 20:45:31 -0700

    Assignment 2

---
+++ lesson2/A2.py
@@ -0,0 +1,12 @@
+name = raw_input("What is your name? ")
+pizzas = raw_input("How many pizzas do you want? ")
+pepperonis = raw_input("How many pepperonis do you want? ")
+olives = raw_input("How many olives do you want? ")
+
+print name + " ordered " + str(pizzas) + " with " + str(pepperonis) + " pepperonis and " + str(olives) + " olives. "
+
+a = float(raw_input("Enter a number. "))
+b = float(raw_input("Enter a second number. "))
+c = float(raw_input("Enter a third number. "))
+
+print str((a+b+c)/3) + " is the average of " + str(a) + ", " + str(b) + ", and " +str(c)
