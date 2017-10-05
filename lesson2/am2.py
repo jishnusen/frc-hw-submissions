ebabd4531f8d48319506d3b40eb0c60fb76ca06a
59846 Carl Csaposs <CC59846@kids.djusd.k12.ca.us>
2017-10-04 19:48:32 -0700

    Finished assignement 2

---
+++ lesson2/am2.py
@@ -0,0 +1,5 @@
+name = raw_input("Enter your name: ")
+	num = raw_input("How many pizzas? ")
+	pep = raw_input("How many pepperonis? ")
+	olv = raw_input("How many olives? ")
+	print(name + " ordered " + num + " pizzas with " + pep + "pepperonis and " + olv + " olives.")
