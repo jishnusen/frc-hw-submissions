45535e13492e134268c100a3e75b650be3ccd577
Alexander <alexdi0421@gmail.com>
2017-10-04 20:39:17 -0700

    Alex's lesson3 hw

---
+++ lesson3/lesson3.py
@@ -0,0 +1,7 @@
+print float(4.6) #prints out 4.6
+print int(4.6) #prints out 4
+print int(3.7) #returns 3
+#print int("4.5") #reports error that 4.5 is not an integer
+#print int("4 and 5") #reports error that "4 and 5" is not an integer
+#print float("three point six") #reports error that "three point six" is not a float
+#print float("three") #reports error that "three" is not a float
