4773355c2e83972beb0547aa49a86fa3a15b2c8f
Alexander <alexdi0421@gmail.com>
2017-10-04 20:37:57 -0700

    Alex's lesson2 hw

---
+++ lesson2/lesson2.py
@@ -0,0 +1,6 @@
+name = raw_input("What is your name: ")
+x = int(raw_input("How many pizzas do you want: "))
+y = int(raw_input("How many pepperonis do you want: "))
+z = int(raw_input("How many olives do you want: "))
+
+print "We will order",str(x),"pizzas,",str(y),"pepperonis and",str(z),"olives for", name
