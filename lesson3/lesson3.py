de55e74f07fd2cf0aa513f95a4be0842a804cea8
Hanson <hzuoli@gmail.com>
2017-10-04 20:40:45 -0700

    completed assignment 3

---
+++ lesson3/lesson3.py
@@ -0,0 +1,4 @@
+print "the difference between float(4.6) and int(4.6) is that the float prints 4.6 and the int prints 4 because the integer of 4.6 is 4."
+print "int(3.7) prints " + str(int(3.7))
+print "print int('4.5') and print int('4 and 5') dont work."
+print "print float('three point six') and print float('three') dont work."
