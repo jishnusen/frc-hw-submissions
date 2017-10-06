fb64f838a6c78bc6d983e97ed82abd6cc66b0981
lucas aranda <lucaskart07@gmail.com>
2017-10-05 18:43:07 -0700

    Finished lesson 2

---
+++ lesson2/lesson2.py
@@ -0,0 +1,6 @@
+name = raw_input("What is your name?")
+amount = raw_input("How many pizzas would you like to order?")
+pep = raw_input("How many slices of pepperoni would you like on your pizza?")
+oli = raw_input("Would you like some olives? If so enter amount.")
+print name + " has ordered " + amount + " pizza(s) with " + pep + " slices of pepperoni and " + oli + " olives."
+print "Thank you come again!"
