25347e78b01c43722cf740dd73edb4247741dc1a
Alexander <alexdi0421@gmail.com>
2017-10-10 19:03:39 -0700

    Alex's lesson7part2 hw

---
+++ lesson7/lesson7part2.py
@@ -0,0 +1,7 @@
+def divisble(x,y):
+	if x%y == 0:
+		print "These numbers are divisble"
+	else:
+		print "These numbers not are divisble"
+
+divisble(float(raw_input("Enter a number: ")), float(raw_input("Enter a number: ")))
