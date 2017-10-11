30cf4ed7da54627407657d5799ac6d4f6d8fa523
rebeccaAhirsch <literaturecool@gmail.com>
2017-10-10 21:35:04 -0700

    completed hw2

---
+++ lesson2/hw_2.py
@@ -0,0 +1,9 @@
+x = raw_input("how many pizzas do you want?")
+z = raw_input("how many olives do you want?")
+y = raw_input("how many pieces of pepperoni do you want?")
+
+
+print "you ordered:"
+print x + " pizzas"
+print z + " olives"
+print y + " pepperoni"
