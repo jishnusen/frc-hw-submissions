a5464c00ed0e78a61f4ea23610afde2b2e4215f7
rebeccaAhirsch <literaturecool@gmail.com>
2017-10-10 21:50:58 -0700

    completed hw 4

---
+++ lesson4/hw_4.py
@@ -0,0 +1,17 @@
+age = float(raw_input("what is your age?"))
+def ability(age):
+	if age>=14:
+		print "You can join the robotics team"
+	else:
+		print "R.I.P. you can't do anything"
+	if age>=16:
+		print "you can drive and get a job"
+	if age>=18:
+		print "you can attend college"
+	if age>=21:
+		print "You are an adult :("
+	if age>=35: 
+		print "You can become President"
+	
+
+ability(age)
