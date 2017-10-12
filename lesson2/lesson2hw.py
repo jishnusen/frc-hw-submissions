8679d39c689a96e6badc96794e362ef8f807c3a0
Amena <amena@buba.local>
2017-10-11 20:21:02 -0700

    Lesson 2 complete

---
+++ lesson2/lesson2hw.py
@@ -0,0 +1,11 @@
+name = raw_input("What is your name? ")
+
+num_pizza = raw_input("How many pizzas would you like? ")
+
+num_pepe = raw_input("How many pepperonies would you like on each pizza? ")
+
+num_olive = raw_input("How many olives would you like on each pizza? ")
+
+print "Hello! Welcome to Amiiba's pizzaria, " + name + " you have ordered " + num_pizza + " pizzas; " + num_pepe + " pepperonis and " + num_olive + " olives on the pizza(s)."
+
+print "Thank you for shopping at SavMartss"
