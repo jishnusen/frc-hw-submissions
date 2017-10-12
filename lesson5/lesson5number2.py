e697672458f15846565ebb818d395a1306dd27e4
Siddarth Merchant <32380389+SMerchant1678@users.noreply.github.com>
2017-10-11 17:02:41 -0700

    I finished Lesson5Part2

---
+++ lesson5/lesson5number2.py
@@ -0,0 +1,5 @@
+name = raw_input("What is your name? ")
+color = raw_input("What is your favorite color? ")
+pet = raw_input("How many pets do you have? ")
+array = [name, color, pet]
+print array[0] + "'s favorite color is " + array[1] + ". They have " + array[2] + " pets."
