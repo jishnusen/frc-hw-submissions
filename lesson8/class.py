8ec52fe9a7213b6d5ed11724a6bf42e3c405f371
59254 Laura Rae Duffy <LD59254@kids.djusd.k12.ca.us>
2017-10-11 20:44:10 -0700

    Assignment 8 (unfinished)

---
+++ lesson8/class.py
@@ -0,0 +1,31 @@
+# I am kind of confused about this and am not sure how to do it 
+# but I am uploading it anyway
+class Robot:
+	def_init_(self, pos, arm_pos, piece, score_init):
+		self.pos = pos
+		arm.pos = arm_pos
+		self.piece = piece
+		self.score_init = score_init
+	def move(pos, dist):
+		dist = a
+		pos += dist
+	def lift(arm_pos, height):
+		height = b
+		arm_pos += height
+	def score(score_init, score):
+		score_init == 0
+		score += score_init
+	def piece(piece):
+		peice == False
+
+while 1== 1:
+	if pos == 3:
+		piece == True
+		print "You have a piece. "
+	while self.pos != 7 and arm.pos != 10:
+		a = raw_input("Enter a number to move the robot. ")
+		b = raw_input("Enter a number to lift the robot's arm. ")
+		move(0,a)
+		lift(0,b)
+	if self.pos == 7 and arm.pos == 10:
+		score == 1
