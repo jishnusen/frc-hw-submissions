918e334471862a225f1b0982007bb7419f86dd92
jasonguo9019 <jasonzguo2016@gmail.com>
2017-10-11 20:36:55 -0700

    completed homework

--- lesson4/lesson4.5.py
+++ lesson4/lesson4.5.py
@@ -1,7 +1,7 @@
 patients = [[70, 1.8], [80, 1.9], [150, 1.7]]
 def calculate_bmi(weight, height):		
   return weight / (height ** 2)
-for patient in patients:
-  weight, height = patients[0]
-bmi = calculate_bmi(height, weight)
+for patient in patients: 		
+ 	weight, height = patients[0]	
+	bmi = calculate_bmi(height, weight)	   
 print "Patient's BMI is:" + str(bmi)
