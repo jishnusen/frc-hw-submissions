68e659bc274468fdf4b6f6c842ed4947e859eb2b
apurvagyan <apurvagyan@gmail.com>
2017-10-12 19:36:32 -0700

    1-5

---
+++ lesson3/hw3.py
@@ -0,0 +1,11 @@
+#Lesson 3 HW
+
+print float(4.6)
+print int(4.6)
+ #float == int
+
+print "See, the float 4.6 is not the same as the int 4.6."
+
+Look = int(3.7)
+print Look
+print "When we make 3.7 an int it makes the number", Look
