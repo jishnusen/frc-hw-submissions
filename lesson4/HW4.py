5b1eabe7bf2af4c11dc611865d42ca552a00ffac
aakamishra <aakamishra1@gmail.com>
2017-10-12 18:59:10 -0700

    Finished Hw

---
+++ lesson4/HW4.py
@@ -0,0 +1,13 @@
+print("Welcome to our adoption agency")
+size_house = float ( print ("How big is your house?"))
+earnings = int (print ("How much do you earn per year?"))
+how_many = int (print("How many dogs do you want?"))
+    if size_house >= 25000:
+        v1 = True
+    elif earnings >= 20000:
+        v2 = True
+    elif how_many >= 1:
+        v3 = True
+def question_set (parameter1, parameter2, parameter3):
+
+    
