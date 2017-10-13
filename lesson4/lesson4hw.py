9a70872d6e74da85fd655dad6a8be04bdbd516b1
Amena <amena@buba.local>
2017-10-12 19:21:24 -0700

    adding lesson four

---
+++ lesson4/lesson4hw.py
@@ -0,0 +1,6 @@
+age = int(raw_input("How old are you?" ))
+
+if age < 14:
+	print "YOU MAY *NOT* JOIN THE ROBOTICS TEAM!! GO AWAY!! try again when you're 14..."
+else:
+	print "You *MAY* join the team!! Congrats!"
