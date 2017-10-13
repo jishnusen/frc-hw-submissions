18e8432efb66bce65b87d84f4721dae9040ea92b
KateUnger <32380357+KateUnger@users.noreply.github.com>
2017-10-12 19:55:41 -0700

    Finished HW#4

--- lesson6/HW6.py
+++ lesson6/HW6.py
@@ -8,3 +8,14 @@ while inputs != "stop" and inputs != "Stop":
 	if inputs != "stop" and inputs != "Stop":
 		array.append(inputs)
 print array
+
+print ""
+print ""
+
+variable = 5
+array3 = []
+while len(array3) < 5:
+	array2 = [1, 2, 3, 4, 5]
+	array3.append(variable)
+	variable -= 1
+print array3
